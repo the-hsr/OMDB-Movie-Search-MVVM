c265b85d9d92a1a412373bf2594e187eaf5e711a
the-hsr <himanshu.arunkumar.singh@gmail.com>
2025-05-05 22:48:35 +0530

    Initial Commit

---
+++ OMDB/OMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMDBApp.swift
+//  OMDB
+//
+//  Created by Himanshu Singh on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct OMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OMDBTests/OMDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  OMDBTests.swift
+//  OMDBTests
+//
+//  Created by Himanshu Singh on 05/05/25.
+//
+
+import Testing
+@testable import OMDB
+
+struct OMDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
